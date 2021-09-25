cc2768109b9d18ed834b698cab7c78712c3e4a3d
colibrishin <colibrishin@gmail.com>
2021-09-25 18:14:02 +0900

    Revert Resolution, unlock wb, ae, add requirements for gpio

--- build.sh
+++ build.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+git clone https://github.com/NVIDIA/jetson-gpio.git
+cd ./jetson-gpio
+sudo groupadd -f -r gpio
+sudo usermod -a -G gpio ${USER}
+
+sudo cp lib/python/Jetson/GPIO/99-gpio.rules /etc/udev/rules.d/
+sudo udevadm control --reload-rules && sudo udevadm trigger
+
+cd ../
+rm -rf jetson-gpio
+
 sudo docker-compose build
 sudo docker image prune
--- trt/video_stream.py
+++ trt/video_stream.py
@@ -3,27 +3,27 @@ gi.require_version('Gst', '1.0')
 from gi.repository import Gst
 import numpy as np
 
-LAUNCH_PIPELINE = 'nvarguscamerasrc sensor-id=0 name=cam0 aelock=true awblock=true wbmode=0 ! \
+LAUNCH_PIPELINE = 'nvarguscamerasrc sensor-id=0 name=cam0  ! \
     video/x-raw(memory:NVMM), width=(int)1280, height=(int)720, format=(string)NV12, framerate=(fraction)60/1 ! \
     nvvidconv flip-method=0 ! video/x-raw,width=640,height=480,format=BGRx ! videoconvert ! \
-    video/x-raw,width=640,height=480,format=RGB ! videoscale ! \
-    video/x-raw,width=320,height=240,format=RGB ! videoconvert ! \
+    video/x-raw,width=640,height=480,format=RGB ! videoconvert ! \
     tee name=t ! queue leaky=downstream max-size-buffers=1 ! appsink max-buffers=1 \
     t. ! queue leaky=downstream max-size-buffers=1 ! videoconvert ! jpegenc ! multifilesink location=taken.jpg'
 
-LAUNCH_PIPELINE_WO_FILE = 'nvarguscamerasrc sensor-id=0 name=cam0 aelock=true awblock=true wbmode=0 ! \
+LAUNCH_PIPELINE_WO_FILE = 'nvarguscamerasrc sensor-id=0 name=cam0  ! \
     video/x-raw(memory:NVMM), width=(int)1280, height=(int)720, format=(string)NV12, framerate=(fraction)60/1 ! \
     nvvidconv flip-method=0 ! video/x-raw,width=640,height=480,format=BGRx ! videoconvert ! \
-    video/x-raw,width=640,height=480,format=RGB ! videoscale ! \
-    video/x-raw,width=320,height=240,format=RGB ! videoconvert ! \
+    video/x-raw,width=640,height=480,format=RGB ! videoconvert ! \
     queue leaky=downstream max-size-buffers=1 ! appsink max-buffers=1'
 
-INDEX_CAPTURE_PIPELINE = 'nvarguscamerasrc sensor-id=0 name=cam0 aelock=true awblock=true wbmode=0 ! \
+INDEX_CAPTURE_PIPELINE = 'nvarguscamerasrc sensor-id=0 name=cam0  ! \
     video/x-raw(memory:NVMM), width=(int)1280, height=(int)720, format=(string)NV12, framerate=(fraction)60/1 ! \
     nvvidconv flip-method=0 ! video/x-raw,width=640,height=480,format=BGRx ! videoconvert ! \
     video/x-raw,width=640,height=480,format=RGB ! videoconvert ! \
     tee name=t ! queue leaky=downstream max-size-buffers=1 ! appsink max-buffers=1 \
     t. ! queue leaky=downstream max-size-buffers=1 ! videoconvert ! jpegenc ! multifilesink location=capture/taken-%00005d.jpg'
+
+# This can be improved by using Gst.ElementFactory.
 
 def start_gst(pipeline:str):
     Gst.init(None)
