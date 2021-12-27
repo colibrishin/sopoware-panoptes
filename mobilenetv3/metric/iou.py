e4303b5dbcbcd0c1d0dc0f68b9108beed86942b4
colibrishin <5135974+colibrishin@users.noreply.github.com>
2021-12-27 16:34:28 +0900

    IoU Fixed

--- mobilenetv3/metric/iou.py
+++ mobilenetv3/metric/iou.py
@@ -1,112 +1,51 @@
-from PIL import Image
-import numpy as np
 import tensorflow as tf
+import numpy as np
 
-class IoU(tf.keras.metrics.Metric):
-  def __init__(self, n_classes: int, name='IoU', **kwargs):
-    super(IoU, self).__init__(name=name, **kwargs)
-    self.union = self.add_weight(name='iou_union', initializer='zeros')
-    self.intersection = self.add_weight(name='iou_intersection', initializer='zeros')
-    self.n_classes = n_classes
+class MeanIoUArgMax(tf.keras.metrics.MeanIoU):
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        return super().update_state(y_true, tf.argmax(y_pred, axis=-1), sample_weight)
 
-  def split_channel(self, y):
-    arrays = []
-    for i in range(0, self.n_classes):
-      arrays.append(y == i)
+class MeanIoUArgMaxWithClass(MeanIoUArgMax):
+  '''
+  From Keras MeanIoU, Argmax the y_pred
+  pick one class IoU
+  '''
+  def __init__(self, num_classes, n_class, name='MeanIoUArgMaxWithClass', dtype=None):
+    super(MeanIoUArgMaxWithClass, self).__init__(name=name, dtype=dtype)
+    self.n_class = n_class
+    self.num_classes = num_classes
 
-    return tf.stack(arrays, axis=-1)
-
-  def update_state(self, y_true, y_pred, sample_weight=None):
-    y_true = tf.squeeze(y_true, axis=-1)
-    y_true = self.split_channel(y_true)
-    y_true = tf.cast(y_true, tf.bool)
-
-    y_pred = tf.argmax(y_pred, axis=-1)
-    y_pred = self.split_channel(y_pred)
-    y_pred = tf.cast(y_pred, tf.bool)
-    
-    y_true_count = tf.reduce_sum(tf.cast(y_true, tf.float32)) 
-    y_pred_count = tf.reduce_sum(tf.cast(y_pred, tf.float32))
-    
-    intersection = tf.logical_and(y_true, y_pred)
-    union = tf.logical_or(y_true, y_pred)
-
-    intersection = tf.reduce_sum(tf.cast(intersection, tf.float32))
-    union = tf.reduce_sum(tf.cast(union, tf.float32))
-
-    self.intersection.assign_add(intersection)
-    self.union.assign_add(union)
+    # Variable to accumulate the predictions in the confusion matrix.
+    self.total_cm = self.add_weight(
+        'total_confusion_matrix',
+        shape=(num_classes, num_classes),
+        initializer=tf.compat.v1.zeros_initializer)
 
   def result(self):
-    return self.intersection / self.union
+    """Compute the mean intersection-over-union via the confusion matrix."""
+    sum_over_row = tf.cast(
+        tf.reduce_sum(self.total_cm, axis=0), dtype=self._dtype)[self.n_class]
+    sum_over_col = tf.cast(
+        tf.reduce_sum(self.total_cm, axis=1), dtype=self._dtype)[self.n_class]
+    true_positives = tf.cast(
+        tf.linalg.tensor_diag_part(self.total_cm), dtype=self._dtype)[self.n_class]
 
-class RoadIoU(tf.keras.metrics.Metric):
-  def __init__(self, road_class: int, name='RoadIoU', **kwargs):
-    super(RoadIoU, self).__init__(name=name, **kwargs)
-    self.union = self.add_weight(name='iou_union', initializer='zeros')
-    self.intersection = self.add_weight(name='iou_intersection', initializer='zeros')
+    # sum_over_row + sum_over_col =
+    #     2 * true_positives + false_positives + false_negatives.
+    denominator = sum_over_row + sum_over_col - true_positives
 
-    self.ROAD_CLASS = road_class
+    # The mean is only computed over classes that appear in the
+    # label or prediction tensor. If the denominator is 0, we need to
+    # ignore the class.
+    num_valid_entries = tf.reduce_sum(
+        tf.cast(tf.not_equal(denominator, 0), dtype=self._dtype))
 
-  def split_channel(self, y):
-    arrays = []
-    arrays.append(y == self.ROAD_CLASS)
+    iou = tf.math.divide_no_nan(true_positives, denominator)
 
-    return tf.stack(arrays, axis=-1)
+    return tf.math.divide_no_nan(
+        tf.reduce_sum(iou, name='mean_iou'), num_valid_entries)
 
-  def update_state(self, y_true, y_pred, sample_weight=None):
-    y_true = tf.squeeze(y_true, axis=-1)
-    y_true = self.split_channel(y_true)
-    y_true = tf.cast(y_true, tf.bool)
-
-    y_pred = tf.argmax(y_pred, axis=-1)
-    y_pred = self.split_channel(y_pred)
-    y_pred = tf.cast(y_pred, tf.bool)
-
-    intersection = tf.logical_and(y_true, y_pred)
-    union = tf.logical_or(y_true, y_pred)
-
-    intersection = tf.reduce_sum(tf.cast(intersection, tf.float32))
-    union = tf.reduce_sum(tf.cast(union, tf.float32))
-
-    self.intersection.assign_add(intersection)
-    self.union.assign_add(union)
-
-  def result(self):
-    return self.intersection / self.union
-
-class SidewalkIoU(tf.keras.metrics.Metric):
-
-  def __init__(self, sidewalk_class: int, name='SidewalkIoU', **kwargs):
-    super(SidewalkIoU, self).__init__(name=name, **kwargs)
-    self.union = self.add_weight(name='iou_union', initializer='zeros')
-    self.intersection = self.add_weight(name='iou_intersection', initializer='zeros')
-
-    self.SIDEWALK_CLASS = sidewalk_class
-
-  def split_channel(self, y):
-    arrays = []
-    arrays.append(y == self.SIDEWALK_CLASS)
-
-    return tf.stack(arrays, axis=-1)
-
-  def update_state(self, y_true, y_pred, sample_weight=None):
-    y_true = tf.squeeze(y_true, axis=-1)
-    y_true = self.split_channel(y_true)
-    y_true = tf.cast(y_true, tf.bool)
-
-    y_pred = tf.argmax(y_pred, axis=-1)
-    y_pred = self.split_channel(y_pred)
-    y_pred = tf.cast(y_pred, tf.bool)
-
-    intersection = tf.logical_and(y_true, y_pred)
-    union = tf.logical_or(y_true, y_pred)
-
-    intersection = tf.reduce_sum(tf.cast(intersection, tf.float32))
-    union = tf.reduce_sum(tf.cast(union, tf.float32))
-
-    self.intersection.assign_add(intersection)
-    self.union.assign_add(union)
-    
-  def result(self):
-    return self.intersection / self.union
+  def get_config(self):
+    config = {'num_classes': self.num_classes, 'class': self.n_class}
+    base_config = super(MeanIoUArgMaxWithClass, self).get_config()
+    return dict(list(base_config.items()) + list(config.items()))
